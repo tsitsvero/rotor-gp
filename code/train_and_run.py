34f2b96e3772eb20d76f45a1545ffddfe8cce604
Mikhail (hpc) <>
2023-07-28 19:25:26 +0900

    long run prepared

--- code/train_and_run.py
+++ code/train_and_run.py
@@ -12,7 +12,7 @@
 
 # ## Make sweep
 
-# In[ ]:
+# In[80]:
 
 
 # # https://docs.wandb.ai/guides/sweeps/walkthrough
@@ -42,37 +42,37 @@ wandb.init(project="rotor-gp", save_code=True, notes="hello", id=machine_name, m
 
 # ## Data loading part
 
-# In[2]:
+# In[81]:
 
 
 #Hydrogen: 
-ind_H_1 = [117, 119, 123, 125, 135, 137, 116, 118, 122, 124, 134, 136] 
-ind_H_2 = [115, 121, 127, 129, 133, 139, 132, 138, 126, 128, 114, 120] 
-ind_H_3 = [130, 131, 140, 141, 142, 143, 144, 145, 164, 165, 166, 167, 192, 193, 206, 207, 250, 251] 
-ind_H_4 = [146, 148, 150, 147, 149, 151, 152, 154, 156, 153, 155, 157, 158, 160, 162, 159, 161, 163, 168, 170, 172, 169, 171, 173, 174, 176, 178, 175, 177, 179, 180, 182, 184, 181, 183, 185, 186, 188, 190, 187, 189, 191, 194, 196, 198, 195, 197, 199, 200, 202, 204, 201, 203, 205, 208, 210, 212, 209, 211, 213, 214, 216, 218, 215, 217, 219, 220, 222, 224, 221, 223, 225, 226, 228, 230, 227, 229, 231, 232, 234, 236, 233, 235, 237, 238, 240, 242, 239, 241, 243, 244, 246, 248, 245, 247, 249, 252, 254, 256, 253, 255, 257, 258, 260, 262, 259, 261, 263] 
+ind_H_1 = sorted([117, 119, 123, 125, 135, 137, 116, 118, 122, 124, 134, 136]) 
+ind_H_2 = sorted([115, 121, 127, 129, 133, 139, 132, 138, 126, 128, 114, 120] )
+ind_H_3 = sorted([130, 131, 140, 141, 142, 143, 144, 145, 164, 165, 166, 167, 192, 193, 206, 207, 250, 251] )
+ind_H_4 = sorted([146, 148, 150, 147, 149, 151, 152, 154, 156, 153, 155, 157, 158, 160, 162, 159, 161, 163, 168, 170, 172, 169, 171, 173, 174, 176, 178, 175, 177, 179, 180, 182, 184, 181, 183, 185, 186, 188, 190, 187, 189, 191, 194, 196, 198, 195, 197, 199, 200, 202, 204, 201, 203, 205, 208, 210, 212, 209, 211, 213, 214, 216, 218, 215, 217, 219, 220, 222, 224, 221, 223, 225, 226, 228, 230, 227, 229, 231, 232, 234, 236, 233, 235, 237, 238, 240, 242, 239, 241, 243, 244, 246, 248, 245, 247, 249, 252, 254, 256, 253, 255, 257, 258, 260, 262, 259, 261, 263] )
 
 #Carbon: 
-ind_C_1 = [6, 8, 10, 7, 9, 11] 
-ind_C_2 = [18, 26, 44, 19, 27, 45] 
-ind_C_3 = [24, 28, 16, 20, 42, 46, 17, 21, 25, 29, 43, 47] 
-ind_C_4 = [30, 34, 14, 22, 40, 48, 15, 23, 31, 35, 41, 49] 
-ind_C_5 = [12, 13, 32, 33, 38, 39] 
-ind_C_6 = [36, 50, 64, 37, 51, 65, 52, 54, 80, 53, 55, 81, 62, 74, 96, 63, 75, 97] 
-ind_C_7 = [56, 60, 57, 61, 68, 72, 69, 73, 66, 70, 67, 71, 78, 84, 79, 85, 58, 59, 88, 90, 89, 91, 86, 87, 92, 94, 93, 95, 76, 77, 82, 83, 98, 100, 99, 101] 
+ind_C_1 = sorted([6, 8, 10, 7, 9, 11] )
+ind_C_2 = sorted([18, 26, 44, 19, 27, 45] )
+ind_C_3 = sorted([24, 28, 16, 20, 42, 46, 17, 21, 25, 29, 43, 47] )
+ind_C_4 = sorted([30, 34, 14, 22, 40, 48, 15, 23, 31, 35, 41, 49] )
+ind_C_5 = sorted([12, 13, 32, 33, 38, 39] )
+ind_C_6 = sorted([36, 50, 64, 37, 51, 65, 52, 54, 80, 53, 55, 81, 62, 74, 96, 63, 75, 97] )
+ind_C_7 = sorted([56, 60, 57, 61, 68, 72, 69, 73, 66, 70, 67, 71, 78, 84, 79, 85, 58, 59, 88, 90, 89, 91, 86, 87, 92, 94, 93, 95, 76, 77, 82, 83, 98, 100, 99, 101] )
 
 #Nytrogen: 
-ind_N_1 = [102, 103, 104, 105, 106, 107] 
+ind_N_1 = sorted( [102, 103, 104, 105, 106, 107] )
 
 #Oxygen: 
-ind_O_1 = [108, 109, 110, 111, 112, 113] 
+ind_O_1 = sorted( [108, 109, 110, 111, 112, 113] )
 
 #Silicon: 
-ind_Si_1 = [0, 1, 2, 3, 4, 5]
+ind_Si_1 = sorted( [0, 1, 2, 3, 4, 5] )
 
 len(ind_C_1 + ind_C_2 + ind_C_3 + ind_C_4 + ind_C_5 + ind_C_6 + ind_C_7) + len(ind_H_1 + ind_H_2 + ind_H_3 + ind_H_4)+ len(ind_N_1)+ len(ind_O_1)+ len(ind_Si_1)
 
 
-# In[427]:
+# In[82]:
 
 
 # %%time
@@ -128,7 +128,7 @@ print("Saving data to directory: ", temp_dir)
 
 import sys
 log_file = temp_dir + '/LOG_GPU.log'
-sys.stdout = open(log_file, 'w')
+# sys.stdout = open(log_file, 'w')
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -156,7 +156,7 @@ traj_2100 = io.read(data_folder + "datasets/rotors/different_temperatures/2100/O
 
 # for parameter selection purpose:
 # traj_train = traj_300[100:500:5].copy() #traj_1800.copy() + traj_2100.copy()
-traj_train = traj_1500[100:500:4].copy()
+traj_train = traj_1500[100:500:5].copy()
 # traj_train = traj_2100.copy()
 # training_indices = np.sort(  np.arange(0, 500, 5) )  
 # traj_train = [traj_md[i] for i in training_indices]
@@ -273,7 +273,7 @@ for g in range(len(atomic_groups)):
 
 # ## Training part
 
-# In[424]:
+# In[83]:
 
 
 import numpy as np
@@ -297,20 +297,20 @@ test_F = fdm.test_F
 
 ### Prepare data loaders and specify how to sample data for each group:
 total_samples_per_group = [
-    2_000, # ind_H_1
-    2_000, # ind_H_2
-    2_000, # ind_H_3
-    2_000, # ind_H_4    
-    2_000, # ind_C_1
-    2_000, # ind_C_2
-    2_000, # ind_C_3
-    2_000, # ind_C_4
-    2_000, # ind_C_5
-    2_000, # ind_C_6
-    2_000, # ind_C_7
-    2_000, # ind_N_1
-    2_000, # ind_O_1
-    2_000, # ind_Si_1 
+    1_000, # ind_H_1
+    1_000, # ind_H_2
+    1_000, # ind_H_3
+    1_000, # ind_H_4    
+    1_000, # ind_C_1
+    1_000, # ind_C_2
+    1_000, # ind_C_3
+    1_000, # ind_C_4
+    1_000, # ind_C_5
+    1_000, # ind_C_6
+    1_000, # ind_C_7
+    1_000, # ind_N_1
+    1_000, # ind_O_1
+    1_000, # ind_Si_1 
     ]
 
 high_force_samples_per_group = [
@@ -338,11 +338,15 @@ hparams['train_indices'] = fdm.train_indices
 
 
 
-# In[425]:
+# In[126]:
 
 
 n_steps = 100
-lr = 0.04
+lr = 0.04 # 0.04 for ARD
+
+import logging
+
+logging.getLogger("pytorch_lightning").setLevel(logging.INFO) # logging.ERROR to disable or INFO
 
 model_H_1_hparams = {
     'atomic_group' : ind_H_1,
@@ -662,11 +666,11 @@ AG_force_model.fit()
 
 # ## Testing part
 
-# In[426]:
+# In[96]:
 
 
 ### TESTING PREDICITONS ###
-print('Testing performance with (meta-)dynamics run...')
+
 
 from fande.predict import PredictorASE
 
@@ -687,13 +691,45 @@ predictor = PredictorASE(
 
 rmse_per_model, mae_per_model = predictor.test_errors(view_worst_atoms=True)
 
-rmse_per_model
+# rmse_per_model
 
 
-# In[360]:
+# In[136]:
+
+
+# from fande.predict import PredictorASE
+
+# from fande.ase import FandeCalc
+
+
+
+# predictor = PredictorASE(
+#             fdm,
+#             model_e,
+#             trainer_e,
+#             AG_force_model,
+#             # trainer_f,
+#             hparams,
+#             soap_params
+# )
+
+# atoms = traj_test[14].copy()
+
+# # predictor.predict_forces_single_snapshot_r(atoms)
+
+# atoms = traj_test[5].copy()
+
+# atoms.calc = FandeCalc(predictor)
+
+# atoms.get_forces()
+
+
+# In[133]:
 
 
 ### MD with fande calc
+print('Testing performance with (meta-)dynamics run...')
+
 from fande.ase import FandeCalc
 from ase.units import Bohr,Rydberg,kJ,kB,fs,Hartree,mol,kcal
 
@@ -724,10 +760,10 @@ atoms = traj_test[10].copy()
 atoms.set_pbc(True)
 
 
-moving_atoms = sum(atomic_groups, []) 
-fixed_atoms = list( set(range(264)) - set(moving_atoms) )
-fix_atoms = FixAtoms(indices=fixed_atoms)
-atoms.set_constraint(fix_atoms)
+# moving_atoms = sum(atomic_groups, []) 
+# fixed_atoms = list( set(range(264)) - set(moving_atoms) )
+# fix_atoms = FixAtoms(indices=fixed_atoms)
+# atoms.set_constraint(fix_atoms)
 
 
 atoms.calc = FandeCalc(predictor)
@@ -741,7 +777,7 @@ os.makedirs("md_run/", exist_ok=True)
 # MaxwellBoltzmannDistribution(atoms, temperature_K=300)
 # dyn = VelocityVerlet(
 #     atoms,
-#     dt = 0.5*units.fs,
+#     dt = 0.1*units.fs,
 #     trajectory="md_run/md_test.traj",
 #     logfile="md_run/md_log.log",
 # )
@@ -781,7 +817,7 @@ dyn = Langevin(atoms, 0.2*fs,
                trajectory='md_run/md_test.traj',
                logfile="md_run/md_log.log")
 
-dyn.run(5_000)
+dyn.run(100_000)
 
 # # Structure optimization:
 # dyn = BFGS(
