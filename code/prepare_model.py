e01b6e9b660dbd5c9ace88f9a9787163f9021c38
Mikhail (hpc) <qklmn@manabiya2.local>
2023-11-09 18:26:27 +0900

    atomic groups corrections

--- code/prepare_model.py
+++ code/prepare_model.py
@@ -1,26 +1,49 @@
 
 
-#Hydrogen: 
-ind_H_1 = sorted([117, 119, 123, 125, 135, 137, 116, 118, 122, 124, 134, 136]) 
-ind_H_2 = sorted([115, 121, 127, 129, 133, 139, 132, 138, 126, 128, 114, 120] )
-ind_H_3 = sorted([130, 131, 140, 141, 142, 143, 144, 145, 164, 165, 166, 167, 192, 193, 206, 207, 250, 251] )
-ind_H_4 = sorted([146, 148, 150, 147, 149, 151, 152, 154, 156, 153, 155, 157, 158, 160, 162, 159, 161, 163, 168, 170, 172, 169, 171, 173, 174, 176, 178, 175, 177, 179, 180, 182, 184, 181, 183, 185, 186, 188, 190, 187, 189, 191, 194, 196, 198, 195, 197, 199, 200, 202, 204, 201, 203, 205, 208, 210, 212, 209, 211, 213, 214, 216, 218, 215, 217, 219, 220, 222, 224, 221, 223, 225, 226, 228, 230, 227, 229, 231, 232, 234, 236, 233, 235, 237, 238, 240, 242, 239, 241, 243, 244, 246, 248, 245, 247, 249, 252, 254, 256, 253, 255, 257, 258, 260, 262, 259, 261, 263] )
-#Carbon: 
-ind_C_1 = sorted([6, 8, 10, 7, 9, 11] )
-ind_C_2 = sorted([18, 26, 44, 19, 27, 45] )
-ind_C_3 = sorted([24, 28, 16, 20, 42, 46, 17, 21, 25, 29, 43, 47] )
-ind_C_4 = sorted([30, 34, 14, 22, 40, 48, 15, 23, 31, 35, 41, 49] )
-ind_C_5 = sorted([12, 13, 32, 33, 38, 39] )
-ind_C_6 = sorted([36, 50, 64, 37, 51, 65, 52, 54, 80, 53, 55, 81, 62, 74, 96, 63, 75, 97] )
-ind_C_7 = sorted([56, 60, 57, 61, 68, 72, 69, 73, 66, 70, 67, 71, 78, 84, 79, 85, 58, 59, 88, 90, 89, 91, 86, 87, 92, 94, 93, 95, 76, 77, 82, 83, 98, 100, 99, 101] )
-#Nytrogen: 
-ind_N_1 = sorted( [102, 103, 104, 105, 106, 107] )
-#Oxygen: 
-ind_O_1 = sorted( [108, 109, 110, 111, 112, 113] )
-#Silicon: 
-ind_Si_1 = sorted( [0, 1, 2, 3, 4, 5] )
+# #Hydrogen: 
+# ind_H_1 = sorted([117, 119, 123, 125, 135, 137, 116, 118, 122, 124, 134, 136]) 
+# ind_H_2 = sorted([115, 121, 127, 129, 133, 139, 132, 138, 126, 128, 114, 120] )
+# ind_H_3 = sorted([130, 131, 140, 141, 142, 143, 144, 145, 164, 165, 166, 167, 192, 193, 206, 207, 250, 251] )
+# ind_H_4 = sorted([146, 148, 150, 147, 149, 151, 152, 154, 156, 153, 155, 157, 158, 160, 162, 159, 161, 163, 168, 170, 172, 169, 171, 173, 174, 176, 178, 175, 177, 179, 180, 182, 184, 181, 183, 185, 186, 188, 190, 187, 189, 191, 194, 196, 198, 195, 197, 199, 200, 202, 204, 201, 203, 205, 208, 210, 212, 209, 211, 213, 214, 216, 218, 215, 217, 219, 220, 222, 224, 221, 223, 225, 226, 228, 230, 227, 229, 231, 232, 234, 236, 233, 235, 237, 238, 240, 242, 239, 241, 243, 244, 246, 248, 245, 247, 249, 252, 254, 256, 253, 255, 257, 258, 260, 262, 259, 261, 263] )
+# #Carbon: 
+# ind_C_1 = sorted([6, 8, 10, 7, 9, 11] )
+# ind_C_2 = sorted([18, 26, 44, 19, 27, 45] )
+# ind_C_3 = sorted([24, 28, 16, 20, 42, 46, 17, 21, 25, 29, 43, 47] )
+# ind_C_4 = sorted([30, 34, 14, 22, 40, 48, 15, 23, 31, 35, 41, 49] )
+# ind_C_5 = sorted([12, 13, 32, 33, 38, 39] )
+# ind_C_6 = sorted([36, 50, 64, 37, 51, 65, 52, 54, 80, 53, 55, 81, 62, 74, 96, 63, 75, 97] )
+# ind_C_7 = sorted([56, 60, 57, 61, 68, 72, 69, 73, 66, 70, 67, 71, 78, 84, 79, 85, 58, 59, 88, 90, 89, 91, 86, 87, 92, 94, 93, 95, 76, 77, 82, 83, 98, 100, 99, 101] )
+# #Nytrogen: 
+# ind_N_1 = sorted( [102, 103, 104, 105, 106, 107] )
+# #Oxygen: 
+# ind_O_1 = sorted( [108, 109, 110, 111, 112, 113] )
+# #Silicon: 
+# ind_Si_1 = sorted( [0, 1, 2, 3, 4, 5] )
+
+# # fake assignemnts just to check:
+# #Hydrogen: 
+# ind_H_1 = sorted([117, 119, 123, 125, 135, 137, 116, 118, 122, 124, 134, 136]) 
+# ind_H_2 = sorted([115, 121, 127, 129, 133, 139, 132, 138, 126, 128, 114, 120] )
+# ind_H_3 = sorted([130, 131, 140, 141, 142, 143, 144, 145, 164, 165, 166, 167, 192, 193, 206, 207, 250, 251] )
+# ind_H_4 = sorted([146, 148, 150, 147, 149, 151, 152, 154, 156, 153, 155, 157, 158, 160, 162, 159, 161, 163, 168, 170, 172, 169, 171, 173, 174, 176, 178, 175, 177, 179, 180, 182, 184, 181, 183, 185, 186, 188, 190, 187, 189, 191, 194, 196, 198, 195, 197, 199, 200, 202, 204, 201, 203, 205, 208, 210, 212, 209, 211, 213, 214, 216, 218, 215, 217, 219, 220, 222, 224, 221, 223, 225, 226, 228, 230, 227, 229, 231, 232, 234, 236, 233, 235, 237, 238, 240, 242, 239, 241, 243, 244, 246, 248, 245, 247, 249, 252, 254, 256, 253, 255, 257, 258, 260, 262, 259, 261, 263] )
+# #Carbon: 
+# ind_C_1 = sorted([6, 8, 10, 7, 9, 11] )
+# ind_C_2 = sorted([18, 26, 44, 19, 27, 45] )
+# ind_C_3 = sorted([24, 28, 16, 20, 42, 46, 17, 21, 25, 29, 43, 47] )
+# ind_C_4 = sorted([30, 34, 14, 22, 40, 48, 15, 23, 31, 35, 41, 49] )
+# ind_C_5 = sorted([12, 13, 32, 33, 38, 39] )
+# ind_C_6 = sorted([36, 50, 64, 37, 51, 65, 52, 54, 80, 53, 55, 81, 62, 74, 96, 63, 75, 97] )
+# ind_C_7 = sorted([56, 60, 57, 61, 68, 72, 69, 73, 66, 70, 67, 71, 78, 84, 79, 85, 58, 59, 88, 90, 89, 91, 86, 87, 92, 94, 93, 95, 76, 77, 82, 83, 98, 100, 99, 101] )
+# #Nytrogen: 
+# ind_N_1 = sorted( [102, 103, 104, 105, 106, 107] )
+# #Oxygen: 
+# ind_O_1 = sorted( [108, 109, 110, 111, 112, 113] )
+# #Silicon: 
+# ind_Si_1 = sorted( [0, 1, 2, 3, 4, 5] )
 
 # len(ind_C_1 + ind_C_2 + ind_C_3 + ind_C_4 + ind_C_5 + ind_C_6 + ind_C_7) + len(ind_H_1 + ind_H_2 + ind_H_3 + ind_H_4)+ len(ind_N_1)+ len(ind_O_1)+ len(ind_Si_1)
+
+from icecream import ic
 
 
 
@@ -45,6 +68,7 @@ def prepare_data(hparams, soap_params, traj_sample_rate=1):
         # sys.path.append("../../fande") 
         # sys.path.append("..") 
         import fande
+        from fande.utils.find_atomic_groups import find_atomic_groups
 
         from datetime import datetime
 
@@ -98,8 +122,10 @@ def prepare_data(hparams, soap_params, traj_sample_rate=1):
         # traj_1800 = io.read(data_folder + "datasets/rotors/different_temperatures/1800/OUTCAR", format="vasp-out", index = ":")
         traj_2100 = io.read(data_folder + "datasets/rotors/different_temperatures/2100/OUTCAR", format="vasp-out", index = ":")
 
-        traj_dftb_300 = io.read("/home/qklmn/data/datasets/rotors/different_temperatures/dftb/traj_300.traj", index = ":")
-        traj_dftb_2100 = io.read("/home/qklmn/data/datasets/rotors/different_temperatures/dftb/traj_2100.traj", index = ":")
+        sample_snapshot = traj_2100[0]
+
+        # traj_dftb_300 = io.read("/home/qklmn/data/datasets/rotors/different_temperatures/dftb/traj_300.traj", index = ":")
+        # traj_dftb_2100 = io.read("/home/qklmn/data/datasets/rotors/different_temperatures/dftb/traj_2100.traj", index = ":")
 
         # for parameter selection purpose:
         # traj_train = traj_300[100:500:5].copy() #traj_1800.copy() + traj_2100.copy()
@@ -139,7 +165,8 @@ def prepare_data(hparams, soap_params, traj_sample_rate=1):
         test_data = {'trajectory': traj_test, 'energies': energies_test, 'forces': forces_test}
 
 
-        atomic_groups = [ind_H_1, ind_H_2, ind_H_3, ind_H_4, ind_C_1, ind_C_2, ind_C_3, ind_C_4, ind_C_5, ind_C_6, ind_C_7, ind_N_1, ind_O_1, ind_Si_1] 
+        # atomic_groups = [ind_H_1, ind_H_2, ind_H_3, ind_H_4, ind_C_1, ind_C_2, ind_C_3, ind_C_4, ind_C_5, ind_C_6, ind_C_7, ind_N_1, ind_O_1, ind_Si_1] 
+        atomic_groups = find_atomic_groups(sample_snapshot)
 
         train_centers_positions = sum(atomic_groups, []) #list(range(len(atoms)))
         train_derivatives_positions = sum(atomic_groups, [])#list(range(len(atoms)))
